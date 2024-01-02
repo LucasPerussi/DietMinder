21ed040f512e9eb734ed9c7f0f8fa8ef6b2b0aeb
PC-PERUSSI\perus <perussilucas@hotmail.com>
2024-01-02 18:14:27 -0300

    pm2 dependecies

--- src/routes/nutritionist/index.ts
+++ src/routes/nutritionist/index.ts
@@ -8,8 +8,6 @@ export const routerNutri = Router()
 
 routerNutri.get('/', (req, res) => res.send('API de Nutricionistas'))
 
-
-// List all products
 routerNutri.get('/all', validateJWT, async (req, res) => {
     try {
         const products = await prisma.nutritionists.findMany({})
@@ -20,3 +18,23 @@ routerNutri.get('/all', validateJWT, async (req, res) => {
     }
 });
 
+
+routerNutri.post('/goal/new', validateJWT, async (req, res) => {
+    const { name, type, user, value, targetDate  } = req.body;
+
+    try {
+        const newGoal = await prisma.goals.create({
+            data: {
+                gol_name: name,
+                gol_target_date: targetDate,
+                gol_type: type,
+                gol_value: value,
+                gol_user: user
+            }
+        })
+        res.status(200).send(newGoal);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error fetching your products');
+    }
+});
