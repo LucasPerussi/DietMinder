bea76be99f769e4e2e101efe66aab365fe535349
PC-PERUSSI\perus <perussilucas@hotmail.com>
2024-01-02 22:25:24 -0300

    another try beyond pm

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 // startar o servidor
 import { app } from './app'
 
+console.log("iniciando....")
 const porta = process.env.PORT || 3000
 const server = app.listen(porta, () => console.log(`App ouvindo na porta ${porta}`))
+console.log(`App ouvindo na porta ${porta}`)
 
 process.on('SIGINT', () => {
     server.close()
